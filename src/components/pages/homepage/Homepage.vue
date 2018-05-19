6a5b7a846b30e0fa3aff58626251347d5e56b46f
Cristi Ciobanu <c-ciobanu@protonmail.com>
2018-05-19 18:16:09 +0200

    removed Todo example

--- src/components/pages/homepage/Homepage.vue
+++ src/components/pages/homepage/Homepage.vue
@@ -4,27 +4,20 @@
 			<ShowElement v-for="(show, index) in shows" :key="index" :title="show.title" :poster="show.img"/>
 		</div>
 		<div class="news">
-			<h2>!!!Test</h2>
 		</div>
 		<div class="add-news">
-			<p>!!!Test</p>
 		</div>
 		<div class="add-series">
-			<span>!!!Test</span>
 		</div>
-		<h1>My Todo App!</h1>
-		<TodoList/>
 	</div>
 </template>
 
 <script>
-import TodoList from '@/components/basic/TodoList';
 import ShowElement from '@/components/basic/ShowElement';
 import ShowElementJSON from '@/assets/json/showElement.json';
 
 export default {
   components: {
-    TodoList,
     ShowElement
   },
   data() {
