b33177bd09d9cd7fad4f66ecd37ef5803cb9b219
Cristi Ciobanu <c-ciobanu@protonmail.com>
2018-06-03 12:25:41 +0200

    add mongodb

--- routes/shows.js
+++ routes/shows.js
@@ -1,6 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.get('/', (req, res) => res.send('shows'));
+router.get('/', (req, res) => {
+	req.db.collection('shows').find().toArray(function(err, result) {
+		if (err) {
+			throw err;
+		}
+
+		res.send(result);
+	});
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const app = express();
-const shows = require('./routes/shows')
+const MongoClient = require('mongodb').MongoClient;
+const shows = require('./routes/shows');
 
-// Add headers
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+let database;
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+app.use(function(req, res, next) {
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	// Request methods you wish to allow
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Set to true if you need the website to include cookies in the requests
-    // sent to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Pass to next layer of middleware
-    next();
+	// Set to true if you need the website to include cookies in the requests
+	// sent to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', false);
+
+  // Make database accessible by routers
+  req.db = database;
+
+	// Pass to next layer of middleware
+	next();
 });
 
-app.use('/api/shows', shows)
+app.use('/api/shows', shows);
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+	if (err) {
+    throw err;
+  }
+
+	database = client.db('my-tv-shows-status');
+
+  app.listen(3000, () => console.log('Listening on port 3000'));
+});
