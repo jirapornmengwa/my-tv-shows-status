a3b6c71c7d117710601356a612ca68bf2e55c781
Cristi Ciobanu <c-ciobanu@protonmail.com>
2018-06-17 15:37:55 +0200

    add Header component and measure variables

---
+++ src/components/basic/header/Header.vue
@@ -0,0 +1,28 @@
+<template>
+	<header>
+		<i class="menu"></i>
+	</header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: $measure-height-header;
+  background-color: #f2f2f2;
+
+  .menu {
+    @include icon($icon-bars);
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding-left: $measure-padding-general;
+  }
+}
+</style>
